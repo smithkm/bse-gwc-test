edae37bbfac4f8a7b477abf3d24f30fdd7492af5
Kevin Smith <ksmith@boundlessgeo.com>
2018-05-09 16:37:35 -0700

    Test FileBlobStore

--- testFileBlobstore.rb
+++ testFileBlobstore.rb
@@ -10,10 +10,8 @@ config = Config.read do |config|
   config["xstream_class_remove"]=false
   config["nodes"]=["http://localhost:8080/geoserver/"]
   config["layer"]="a layer with global extent"
-  config["aws_key"]="Enter AWS access key here"
-  config["aws_secret"]="Enter AWS secret key here"
-  config["s3_blobstore_bucket"]="Enter bucket for S3 Blob Store"
-  config["s3_blobstore_prefix"]="Enter prefix for S3 Blob Store"
+  config["file_blobStore_path"]="/tmp/testBlobStore"
+  config["cache_deleted_when_blobstore_changes"]=false
 end
 
 $xstream_class_remove = config["xstream_class_remove"]
@@ -30,17 +28,12 @@ blobstore = "testBlobStore"
 
 blobstore_body = <<EOS
 <?xml version="1.0" encoding="UTF-8"?>
-<S3BlobStore default="false">
+<FileBlobStore default="false">
   <id>#{blobstore}</id>
   <enabled>true</enabled>
-  <bucket>#{config["s3_blobstore_bucket"]}</bucket>
-  <prefix>#{config["s3_blobstore_prefix"]}</prefix>
-  <awsAccessKey>#{config["aws_key"]}</awsAccessKey>
-  <awsSecretKey>#{config["aws_secret"]}</awsSecretKey>
-  <maxConnections>50</maxConnections>
-  <useHTTPS>true</useHTTPS>
-  <useGzip>true</useGzip>
- </S3BlobStore>
+  <baseDirectory>#{config["file_blobStore_path"]}</baseDirectory>
+  <fileSystemBlockSize>4096</fileSystemBlockSize>
+ </FileBlobStore>
 EOS
 
 def assertCacheStatus(response, status)
@@ -68,7 +61,6 @@ nodes.each do |base|
   # set to use default blob store
   rest_update(base+("gwc/rest/layers/"+layer)) do |doc|
     e = doc.root.elements["//blobStoreId"]
-    puts doc
     e.remove unless e.nil?
     doc
   end
@@ -89,27 +81,86 @@ nodes.each do |base|
   end
 end
 
+
+wmts_gettile(node1, layer, gridset, "image/png", 2,1,2) do |response|
+  response.value
+  assertCacheStatus(response, :miss)
+  assertImage(response, :png, 256)
+end
+
+wmts_gettile(node2, layer, gridset, "image/png", 2,1,2) do |response|
+  response.value
+  assertCacheStatus(response, :hit)
+  assertImage(response, :png, 256)
+end
+
+wmts_gettile(node2, layer, gridset, "image/png", 2,1,3) do |response|
+  response.value
+  assertCacheStatus(response, :miss)
+  assertImage(response, :png, 256)
+end
+
 # Add store
 
 rest_add(node1+("gwc/rest/blobstores/"+blobstore), blobstore_body)
-
-wmts_gettile(node1, layer, gridset, "image/png", 2,1,0) do |response|
-  response.value
-  assertCacheStatus(response, :miss)
-  assertImage(response, :png, 256)
-end
-
-wmts_gettile(node2, layer, gridset, "image/png", 2,1,0) do |response|
-  response.value
-  assertCacheStatus(response, :miss)
-  assertImage(response, :png, 256)
-end
 
 # Check that the store was added
 
 nodes.each do |base|
   raise "#{blobstore} not added" if rest_get(base+("gwc/rest/blobstores")).root.elements["/blobStores/blobStore/name[text()='#{blobstore}']"].nil?
   raise "#{blobstore} not added" if rest_get(base+("gwc/rest/blobstores/"+blobstore)).root.elements["/FileBlobStore/id[text()='#{blobstore}']"].nil?
+end
+
+# Set layer to use gridset
+rest_update(node1+("gwc/rest/layers/"+layer)) do |doc|
+  doc.root.add_element(REXML::Element.new("blobStoreId")).text=blobstore
+  doc
+end
+
+wmts_gettile(node1, layer, gridset, "image/png", 2,1,2) do |response|
+  response.value
+  assertCacheStatus(response, :miss)
+  assertImage(response, :png, 256)
+end
+
+wmts_gettile(node2, layer, gridset, "image/png", 2,1,2) do |response|
+  response.value
+  assertCacheStatus(response, :hit)
+  assertImage(response, :png, 256)
+end
+
+wmts_gettile(node2, layer, gridset, "image/png", 2,1,3) do |response|
+  response.value
+  assertCacheStatus(response, :miss)
+  assertImage(response, :png, 256)
+end
+
+if config["cache_deleted_when_blobstore_changes"]
+  # Revert to default
+  
+  rest_update(node1+("gwc/rest/layers/"+layer)) do |doc|
+    e = doc.root.elements["//blobStoreId"]
+    e.remove unless e.nil?
+    doc
+  end
+  
+  wmts_gettile(node1, layer, gridset, "image/png", 2,1,2) do |response|
+    response.value
+    assertCacheStatus(response, :miss)
+    assertImage(response, :png, 256)
+  end
+  
+  wmts_gettile(node2, layer, gridset, "image/png", 2,1,2) do |response|
+    response.value
+    assertCacheStatus(response, :hit)
+    assertImage(response, :png, 256)
+  end
+  
+  wmts_gettile(node2, layer, gridset, "image/png", 2,1,3) do |response|
+    response.value
+    assertCacheStatus(response, :miss)
+    assertImage(response, :png, 256)
+  end
 end
 
 puts "PASS"
